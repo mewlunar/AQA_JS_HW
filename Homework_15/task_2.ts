db8b40ae44b8ba636203c97e84a239f7070a4b59
Tanya <ignatova2499@gmail.com>
2024-11-08 00:02:36 +0400

    Update task_2  hw 15

--- Homework_15/task_2.ts
+++ Homework_15/task_2.ts
@@ -4,16 +4,22 @@
 //     где каждый каждый элемент - это элемент исходного массива умноженный на его индекс
 // Пример:
 //     map([1,2,3,4,5], callback) => [0,2,6,12,20]
+type MapCallBack<T, U> = (value: T, index: number, array: T[]) => U;
+
 function customMap<T, U>(
   array: T[],
-  callback: (element: T, index: number) => U,
+  callbackFunc: MapCallBack<T,U>,
 ): U[] {
   const result: U[] = [];
+
   for (let i = 0; i < array.length; i++) {
-    result.push(callback(array[i], i));
+    result.push(callbackFunc(array[i], i, array));
   }
+
   return result;
 }
 
 const arr: number[] = [1, 2, 3, 4, 5, 6, 7];
+
+const callback: MapCallBack<number, number> = (value,index) =>value*index
 console.log(customMap(arr, (element, index) => element * index));
